bc8416a4120a0acce72d21ea4d78dc40fdeb7f83
ashannon828 <ashannon828@gmail.com>
2020-11-10 23:51:51 +0200

    added image to og

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,15 +1,17 @@
+import expatriantLogo from './public/img/expatriant_logo.png'
+
 export default {
   title: "Jobs in Russia - Expatriant",
   description: "Browse sales, marketing, translating, editing and English teaching jobs in Russia.",
   openGraph: {
     type: 'website',
     locale: 'en_IE',
-    url: 'https://jobs.expatriant.com/',
+    url: expatriantLogo,
     site_name: 'Jobs in Russia - Expatriant',
     images: [
       {
         url: 'public/img/expatriant_logo.png',
-        width: 800,
+        width: 600,
         height: 600,
         alt: 'Expatriant Logo',
       }
